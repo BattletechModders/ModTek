56a5567863251f533cf00ee84569ad8f1d7ecbb6
Michael P. Starkweather <mpstark@gmail.com>
2018-05-04 14:16:08 -0500

    Split Loading code into own class and file, rename core code

--- ModTek/Core.cs
+++ ModTek/Core.cs
@@ -11,58 +11,13 @@ using Newtonsoft.Json;
 
 namespace ModTek
 {
-    public static class ModTek
+    public static class Core
     {
         public static string ModDirectory { get; private set; }
 
         private static string logPath;
         private static string MOD_JSON_NAME = "mod.json";
-
-        public static void LoadMod(ModDef modDef, StreamWriter logWriter = null)
-        {
-            logWriter.WriteLine("Attempting to load {0}", modDef.Name);
-
-            // load mod dll
-            if (modDef.DLL != null )
-            {
-                var dllPath = Path.Combine(modDef.Directory, modDef.DLL);
-
-                if (File.Exists(dllPath))
-                {
-                    if (modDef.DLLEntryPoint == null)
-                    {
-                        BTModLoader.LoadDLL(dllPath, logWriter);
-                    }
-                    else
-                    {
-                        string typeName = null;
-                        string methodName = "Init";
-                        int pos = modDef.DLLEntryPoint.LastIndexOf('.');
-
-                        if (pos == -1)
-                            methodName = modDef.DLLEntryPoint;
-                        else
-                        {
-                            typeName = modDef.DLLEntryPoint.Substring(0, pos - 1);
-                            methodName = modDef.DLLEntryPoint.Substring(pos + 1);
-                        }
-
-                        // TODO: pass in param for modDef.Path, and modDef.Settings if available
-                        BTModLoader.LoadDLL(dllPath, logWriter, methodName, typeName);
-                    }
-                }
-                else
-                {
-                    logWriter.WriteLine("{0} has a DLL specified ({1}), but it's missing! Aborting load.", modDef.Name, dllPath);
-                    return;
-                }
-            }
-            
-            // load other parts of mod!
-
-            logWriter.WriteLine("Loaded {0}", modDef.Name);
-        }
-
+        
         // ran by BTML
         public static void Init()
         {
@@ -111,7 +66,7 @@ namespace ModTek
                 // TODO: load mods in the correct order
                 foreach (var modDef in modDefs)
                 {
-                    LoadMod(modDef, logWriter);
+                    Loading.LoadMod(modDef, logWriter);
                 }
             }
         }
