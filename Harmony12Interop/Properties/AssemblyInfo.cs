0d8e8866258ad89f5ee216b0dd35f72ecb826f99
ghorsington <neigh@coder.horse>
2020-12-18 14:31:54 +0200

    init

---
+++ Harmony12Interop/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Harmony12Interop")]
+[assembly: AssemblyProduct("Harmony12Interop")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
