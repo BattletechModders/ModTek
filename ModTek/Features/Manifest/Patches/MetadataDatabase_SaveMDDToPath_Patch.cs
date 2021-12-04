d6a1db5fb85fe41000c1d8516668f122a6988b2d
CptMoore <39010654+CptMoore@users.noreply.github.com>
2021-12-04 10:08:35 +0100

    Added mdd save tracing patch.

---
+++ ModTek/Features/Manifest/Patches/MetadataDatabase_SaveMDDToPath_Patch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BattleTech.Data;
+using Harmony;
+using static ModTek.Features.Logging.MTLogger;
+
+namespace ModTek.Features.Manifest.Patches
+{
+    [HarmonyPatch(typeof(MetadataDatabase), nameof(MetadataDatabase.SaveMDDToPath))]
+    internal static class MetadataDatabase_SaveMDDToPath_Patch
+    {
+        public static bool Prepare()
+        {
+            return ModTek.Enabled;
+        }
+
+        [HarmonyPriority(Priority.First)]
+        public static void Prefix(out Guid __state)
+        {
+            __state = Guid.NewGuid();
+            Log($"{__state} MetadataDatabase.SaveMDDToPath was called by:\n{Environment.StackTrace}");
+        }
+
+        [HarmonyPriority(Priority.Last)]
+        public static void Postfix(ref Guid __state)
+        {
+            Log($"{__state} MetadataDatabase.SaveMDDToPath finished being called.");
+        }
+    }
+}
