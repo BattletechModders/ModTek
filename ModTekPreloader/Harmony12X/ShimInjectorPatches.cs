85c74e6a2d76445f2c6567e66c5e0c9913df358f
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-09-26 17:09:15 +0200

    Fixed issue with HBS Utilities.BuildExtensionMethodCacheForType trying to load ModTekPreloader classes that are not supposed to be loaded. Closes #206 and #207.

--- ModTekPreloader/Harmony12X/ShimInjectorPatches.cs
+++ ModTekPreloader/Harmony12X/ShimInjectorPatches.cs
@@ -12,32 +12,36 @@ namespace ModTekPreloader.Harmony12X
         internal static void Register(Preloader preloader)
         {
             _preloader = preloader;
-            Harmony.CreateAndPatchAll(typeof(ShimInjectorPatches), typeof(ShimInjectorPatches).FullName);
+            Harmony.CreateAndPatchAll(typeof(Patches), "ModTekPreloader.Harmony12X");
         }
 
-        [HarmonyPatch(typeof(Assembly), nameof(Assembly.LoadFrom), typeof(string), typeof(bool))]
-        [HarmonyPrefix]
-        private static void LoadFrom_Prefix(ref string assemblyFile)
+        // nesting avoids this being loaded by Utilities.BuildExtensionMethodCacheForType
+        private static class Patches
         {
-            _preloader.InjectShimIfNecessary(ref assemblyFile);
-        }
-
-        [HarmonyPatch(typeof(AppDomain), "LoadAssemblyRaw", MethodType.Normal)]
-        [HarmonyPrefix]
-        private static void LoadAssemblyRaw_Prefix(ref byte[] rawAssembly)
-        {
-            // TODO implement
-        }
-
-        [HarmonyPatch(typeof(AppDomain), "LoadAssemblyRaw", MethodType.Normal)]
-        [HarmonyPostfix]
-        private static void LoadAssemblyRaw_Postfix(ref byte[] rawAssembly, ref Assembly __result)
-        {
-            if (__result != null && (__result.GetName().Name.StartsWith("DMDASM.") || __result.GetName().Name == "MonoMod.Utils.GetManagedSizeHelper"))
+            [HarmonyPatch(typeof(Assembly), nameof(Assembly.LoadFrom), typeof(string), typeof(bool))]
+            [HarmonyPrefix]
+            private static void LoadFrom_Prefix(ref string assemblyFile)
             {
-                return;
+                _preloader.InjectShimIfNecessary(ref assemblyFile);
             }
-            Logger.Log($"Warning: LoadAssemblyRaw called with Assembly {__result?.GetName().Name} of size {rawAssembly.Length} bytes, shimming does not support loading this way." + Environment.NewLine + Environment.StackTrace);
+
+            [HarmonyPatch(typeof(AppDomain), "LoadAssemblyRaw", MethodType.Normal)]
+            [HarmonyPrefix]
+            private static void LoadAssemblyRaw_Prefix(ref byte[] rawAssembly)
+            {
+                // TODO implement
+            }
+
+            [HarmonyPatch(typeof(AppDomain), "LoadAssemblyRaw", MethodType.Normal)]
+            [HarmonyPostfix]
+            private static void LoadAssemblyRaw_Postfix(ref byte[] rawAssembly, ref Assembly __result)
+            {
+                if (__result != null && (__result.GetName().Name.StartsWith("DMDASM.") || __result.GetName().Name == "MonoMod.Utils.GetManagedSizeHelper"))
+                {
+                    return;
+                }
+                Logger.Log($"Warning: LoadAssemblyRaw called with Assembly {__result?.GetName().Name} of size {rawAssembly.Length} bytes, shimming does not support loading this way." + Environment.NewLine + Environment.StackTrace);
+            }
         }
     }
 }
