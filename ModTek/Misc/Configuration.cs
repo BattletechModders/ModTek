aba1504038aa9b06c0fa2f1923d37427717f1dda
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-11-13 20:02:06 +0100

    Made config descriptions appear above their respective fields.

--- ModTek/Misc/Configuration.cs
+++ ModTek/Misc/Configuration.cs
@@ -15,82 +15,81 @@ namespace ModTek.Misc
         internal readonly string _Description = $"When changing any of the listed settings, copy the relevant parts into `{FileUtils.GetRelativePath(ConfigPath)}`.";
 
         [JsonProperty]
-        internal bool ShowLoadingScreenErrors = true;
-        [JsonProperty]
         internal const string ShowLoadingScreenErrors_Description = "TODO";
-
         [JsonProperty]
-        internal bool ShowErrorPopup = true;
+        internal bool ShowLoadingScreenErrors = true;
+
         [JsonProperty]
         internal const string ShowErrorPopup_Description = "TODO";
-
         [JsonProperty]
-        internal bool UseErrorWhiteList = true;
+        internal bool ShowErrorPopup = true;
+
         [JsonProperty]
         internal const string UseErrorWhiteList_Description = "TODO";
-
+        [JsonProperty]
+        internal bool UseErrorWhiteList = true;
         [JsonProperty]
         internal string[] ErrorWhitelist = { "Data.DataManager [ERROR] ManifestEntry is null" };
 
         [JsonProperty]
-        internal bool SearchModsInSubDirectories = true;
-        [JsonProperty]
         internal const string SearchModsInSubDirectories_Description = "Searches recursively all directories for mod.json instead only for directories directly found under Mods. Set to false for pre v2.0 behavior.";
-
         [JsonProperty]
-        internal bool ImplicitManifestShouldMergeJSON = true;
+        internal bool SearchModsInSubDirectories = true;
+
         [JsonProperty]
         internal const string ImplicitManifestShouldMergeJSON_Description = "How JSONs in a mods implicit manifest (StreamingAssets) are being treated.";
-
         [JsonProperty]
-        internal bool ImplicitManifestShouldAppendText;
+        internal bool ImplicitManifestShouldMergeJSON = true;
+
         [JsonProperty]
         internal const string ImplicitManifestShouldAppendText_Description = "How CSVs in a mods implicit manifest (StreamingAssets) are being treated.";
-
         [JsonProperty]
-        internal bool ReplaceResetsMerges = true;
+        internal bool ImplicitManifestShouldAppendText;
+
         [JsonProperty]
         internal const string ReplaceResetsMerges_Description = "If enabled, once a manifest entry gets replaced, all previously queued merges will not apply.";
-
         [JsonProperty]
-        internal bool NormalizeCsvIfAppending = true;
+        internal bool ReplaceResetsMerges = true;
+
         [JsonProperty]
         internal const string NormalizeCsvIfAppending_Description = "Normalize CSV files when merging/appending. Filters out empty lines and adds newlines where appropiate. Duplicate title detection and removal from appending files.";
-
         [JsonProperty]
-        internal float DataManagerUnfreezeDelta = 2f;
+        internal bool NormalizeCsvIfAppending = true;
+
         [JsonProperty]
         internal readonly string DataManagerUnfreezeDelta_Description = $"How often to refresh the UI during loading. Does this by skipping loads every specified amount of seconds.";
-
         [JsonProperty]
-        internal float DataManagerEverSpinnyDetectionTimespan = 30f;
+        internal float DataManagerUnfreezeDelta = 2f;
+
         [JsonProperty]
         internal readonly string DataManagerEverSpinnyDetectionTimespan_Description = $"How long data is not being further processed until it is assumed to be stuck for good. Upon detection it dumps lots of data into the log, but nothing more.";
-
         [JsonProperty]
-        internal bool DelayPrewarmToMainMenu = true;
+        internal float DataManagerEverSpinnyDetectionTimespan = 30f;
+
         [JsonProperty]
         internal const string DelayPrewarmToMainMenu_Description = "Delays executing prewarm requests until entering the main menu and dlc packs are loaded. Prevents choppy intro video and makes sure to include dlc items during prewarm.";
-
         [JsonProperty]
-        internal bool ShowDataManagerStatsInLoadingCurtain = true;
+        internal bool DelayPrewarmToMainMenu = true;
+
         [JsonProperty]
         internal const string ShowDataManagerStatsInLoadingCurtain_Description = "Adds DataManager stats when showing a loading curtain.";
-
         [JsonProperty]
-        internal string[] BlockedMods = { "FYLS" };
+        internal bool ShowDataManagerStatsInLoadingCurtain = true;
+
         [JsonProperty]
         internal const string BlockedMods_Description = "Mods that should not be allowed to load. Useful in cases where those mods would (newly) interfere with ModTek.";
-
         [JsonProperty]
-        internal string[] IgnoreMissingMods = { "FYLS" };
+        internal string[] BlockedMods = { "FYLS" };
+
         [JsonProperty]
         internal const string IgnoreMissingMods_Description = "Ignore the dependency requirement of mods that depend on the listed mods. Useful if e.g. ModTek provides the same functionality as the ignored mods.";
+        [JsonProperty]
+        internal string[] IgnoreMissingMods = { "FYLS" };
 
         [JsonProperty]
-        internal bool HideDescriptionsInLastConfig = true;
+        internal const string MinimalLastConfig_Description = "Hides all null and _Description fields from the generated `last` config file.";
         [JsonProperty]
-        internal const string HideDescriptionsOnLastConfig_Description = "Hides all _Description fields from the generated `last` config file.";
+        internal bool MinimalLastConfig = true;
 
         [JsonProperty]
         internal LoggingSettings Logging = new LoggingSettings();
@@ -133,7 +132,7 @@ namespace ModTek.Misc
                 File.WriteAllText(path, "{}");
             }
 
-            config.WriteConfig(ConfigLastPath, config.HideDescriptionsInLastConfig);
+            config.WriteConfig(ConfigLastPath, config.MinimalLastConfig);
 
             return config;
         }
@@ -146,7 +145,7 @@ namespace ModTek.Misc
             }
         }
 
-        private void WriteConfig(string path, bool hideDescriptions = false)
+        private void WriteConfig(string path, bool minimal = false)
         {
             using (var sw = new StreamWriter(path))
             using (var jw = new JsonTextWriter(sw))
@@ -156,9 +155,9 @@ namespace ModTek.Misc
                 jw.Indentation = 4;
                 var serializer = new JsonSerializer
                 {
-                    NullValueHandling = NullValueHandling.Ignore
+                    NullValueHandling = minimal ? NullValueHandling.Ignore : NullValueHandling.Include
                 };
-                if (hideDescriptions)
+                if (minimal)
                 {
                     serializer.ContractResolver = ShouldSerializeContractResolver.Instance;
                 }
