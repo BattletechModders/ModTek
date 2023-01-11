666b2be0d53cb4c29b8455d4add25701e27c36b0
CptMoore <39010654+CptMoore@users.noreply.github.com>
2023-01-11 19:10:44 +0100

    Added working HarmonyX example mod.

---
+++ examples/HarmonyXMod/Main.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using HBS.Logging;
+
+namespace HarmonyXMod;
+
+public static class Main
+{
+    private static readonly ILog s_log = Logger.GetLogger(nameof(HarmonyXMod));
+    public static void Start()
+    {
+        s_log.Log("Starting");
+
+        Harmony.CreateAndPatchAll(typeof(Main));
+
+        s_log.Log("Started");
+    }
+
+    [HarmonyPatch(typeof(VersionInfo), nameof(VersionInfo.GetReleaseVersion))]
+    [HarmonyPostfix]
+    [HarmonyAfter("io.github.mpstark.ModTek")]
+    static void GetReleaseVersion(ref string __result)
+    {
+        var old = __result;
+        __result = old + $"\nHarmonyXMod";
+    }
+}
