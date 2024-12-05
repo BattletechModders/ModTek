50b3c198c7318b0335da549eaed0c50382cd1982
CptMoore <39010654+CptMoore@users.noreply.github.com>
2024-12-05 12:54:57 +0100

    Switched to custom RingBuffer from ConcurrentQueue.

--- ModTek/Features/Logging/LightWeightBlockingQueue.cs
+++ ModTek/Features/Logging/LightWeightBlockingQueue.cs
@@ -1,63 +1,99 @@
-﻿using System.Collections.Concurrent;
+﻿#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace ModTek.Features.Logging;
 
-// this=40ns BlockingCollection=170ns
-// does not really matter but we anyway get better suited APIs
-internal class LightWeightBlockingQueue<T>
+// (this)RingBuffer with reference types=38ns
+// ConcurrentQueue+custom size tracking+addingComplete=40ns
+// BlockingCollection=170ns
+internal class LightWeightBlockingQueue<T> where T : class
 {
-    private readonly ConcurrentQueue<T> _queue = new();
-        
-    private const int MaxQueueSize = 100_000; // probably about 30MB if full
-    private volatile int _queueSize; // faster than calling _queue.Count
     private volatile bool _addingCompleted; // some way to shut down the thread
 
+    // 100k leads to about ~30MB
+    private const int MaxRingBufferSize = 1 << 16; // power of 2 required by FastModuloMaskForBitwiseAnd
+    private const int MaxQueueSize = MaxRingBufferSize - 1; // Start=End need to be distinguishable
+    // ring buffer is used by Disruptor(.NET), seems to work well for them
+    // typed based exchanges are 56ns (fixed as of .NET 7) hence why we use object based ones
+    private readonly object?[] _ringBuffer = new object?[MaxRingBufferSize];
+    // end - start = size // all indexes are "excluding" basically, 0 means 0 not yet written (or read)
+    // TODO how to avoid douple indexes?
+    private volatile int _nextWritingIndex; // sync in between writers
+    private volatile int _nextReadIndex; // sync between readers -> writers
+
+    // see https://en.wikipedia.org/wiki/Modulo#Performance_issues
+    // Bitwise AND is faster than modulo, just requires size to be power of 2
+    // only gained like 1-2ns though (meaning it is within measurement error...)
+    private const int FastModuloMaskForBitwiseAnd = MaxRingBufferSize - 1;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int Next(int index)
+    {
+        return (index + 1) & FastModuloMaskForBitwiseAnd;
+        // return (index + 1) % MaxRingBufferSize;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int Size(int startIndex, int endIndex)
+    {
+        return (endIndex - startIndex) & FastModuloMaskForBitwiseAnd;
+        // return (endIndex - startIndex + MaxRingBufferSize) % MaxRingBufferSize;
+    }
+
     // returns false if nothing can be dequeued anymore (empty + _addingCompleted)
-    internal bool TryDequeueOrWait(out T item)
+    internal bool TryDequeueOrWait([NotNullWhen(true)] out T? item)
     {
         var spinWait = new SpinWait();
-        while (!_queue.TryDequeue(out item))
+        while (true)
         {
+            var index = _nextReadIndex;
+            if (Size(index, _nextWritingIndex) > 0)
+            {
+                item = Unsafe.As<T?>(Interlocked.Exchange(ref _ringBuffer[index], default));
+                if (!ReferenceEquals(item, default))
+                {
+                    _nextReadIndex = Next(index);
+                    return true;
+                }
+            }
+            
+            spinWait.SpinOnce(); // reader should yield and sleep if nothing comes in after some time
+            
             if (_addingCompleted)
             {
                 // this can still drop logs, very unlikely but possible
                 Thread.Sleep(1);
-                if (_queueSize == 0)
+                if (Size(_nextReadIndex, _nextWritingIndex) == 0)
                 {
+                    item = default;
                     return false;
                 }
             }
-            spinWait.SpinOnce();
         }
-
-        Interlocked.Decrement(ref _queueSize);
-        return true;
     }
 
     // returns false if nothing can be enqueued anymore (_addingCompleted)
     internal bool TryEnqueueOrWait(T item)
     {
-        if (_addingCompleted)
+        while (true)
         {
-            return false;
-        }
-
-        while (_queueSize >= MaxQueueSize)
-        {
-            Thread.SpinWait(4);
-
             if (_addingCompleted)
             {
                 return false;
             }
-        }
-        // a compare exchange + retries in the loop would be more strict, but going over max is ok too (and faster)
-        Interlocked.Increment(ref _queueSize);
 
-        _queue.Enqueue(item);
-        return true;
+            var index = _nextWritingIndex;
+            if (Size(_nextReadIndex, index) < MaxQueueSize)
+            {
+                if (Interlocked.CompareExchange(ref _nextWritingIndex, Next(index), index) == index)
+                {
+                    _ringBuffer[index] = item;
+                    return true;
+                }
+            }
+            
+            Thread.SpinWait(4); // main thread should always try to dispatch asap, never wait that much!
+        }
     }
 
     internal void CompleteAdding()
