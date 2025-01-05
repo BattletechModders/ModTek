46d69e55503672f682e2a80c7a9fd6e380a33ab5
CptMoore <39010654+CptMoore@users.noreply.github.com>
2025-01-05 07:53:28 +0100

    Use MonoMod.Backports BitOperations everywhere.

--- ModTek/Features/Logging/FormattingHelpers.cs
+++ ModTek/Features/Logging/FormattingHelpers.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Runtime.CompilerServices;
 
 namespace ModTek.Features.Logging;
@@ -21,7 +22,7 @@ internal static class FormattingHelpers
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static int CountDigits(uint value)
     {
-        var tableValue = s_countDigitsTable[Log2(value)];
+        var tableValue = s_countDigitsTable[BitOperations.Log2(value)];
         return (int)((value + tableValue) >> 32);
     }
     // Algorithm based on https://lemire.me/blog/2021/06/03/computing-the-number-of-digits-of-an-integer-even-faster.
@@ -59,39 +60,5 @@ internal static class FormattingHelpers
         41949672960,
         42949672960,
         42949672960,
-    ];
-
-    // following from MonoMod.Backports
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static int Log2(uint value)
-    {
-        // The 0->0 contract is fulfilled by setting the LSB to 1.
-        // Log(1) is 0, and setting the LSB for values > 1 does not change the log2 result.
-        value |= 1;
-
-        // Fallback contract is 0->0
-        return Log2SoftwareFallback(value);
-    }
-    private static int Log2SoftwareFallback(uint value)
-    {
-        // No AggressiveInlining due to large method size
-        // Has conventional contract 0->0 (Log(0) is undefined)
-
-        // Fill trailing zeros with ones, eg 00010010 becomes 00011111
-        value |= value >> 01;
-        value |= value >> 02;
-        value |= value >> 04;
-        value |= value >> 08;
-        value |= value >> 16;
-
-        var offset = (value * 0x07C4ACDDu) >> 27;
-        return s_log2DeBruijn[offset];
-    }
-    private static readonly byte[] s_log2DeBruijn =
-    [
-        00, 09, 01, 10, 13, 21, 02, 29,
-        11, 14, 16, 18, 22, 25, 03, 30,
-        08, 12, 20, 28, 15, 17, 24, 07,
-        19, 27, 23, 06, 26, 05, 04, 31
     ];
 }
