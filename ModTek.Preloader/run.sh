ed89e70ee9b08529b74010be4d7976b945108f39
CptMoore <39010654+cptmoore@users.noreply.github.com>
2025-10-31 18:21:23 +0100

    Simplified Steam insertion, macOS preload and Linux env variables.

--- ModTek.Preloader/run.sh
+++ ModTek.Preloader/run.sh
@@ -1,34 +1,56 @@
-#!/bin/sh
-set -eux
+#!/usr/bin/env bash
 # Doorstop run script copied from BepInEx and optimized for ModTek
+# macOS: bash 3.2
+# Steam on Linux: bash 4+
+set -eux
 
 executable_name="BattleTech"
-steam_launch_wrapper_name="steam-launch-wrapper" # TODO how does MacOS work?
 
 # Use POSIX-compatible way to get the directory of this script
 a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BASEDIR=$(cd "$a" || exit; pwd -P)
 
-# If launched via Steam, makes sure to delay preloading/lib insertion to just before calling BattleTech,
-# in order to avoid interfering with steam wrappers.
-# > steam -> run (this) -> steam wrappers -> run (continuation) -> BattleTech
-# Steam debugging
-# ./run.sh %command% > ~/steam_command_output.txt 2>&1
-# POSIX shell can't find and insert in the middle of the argument list
-if [ -n "${1:-}" ] && [ "$1" != "${1%"/${steam_launch_wrapper_name}"}" ]
-then
-  steam_first_wrapper="$1"
-  set -- "$@" "$1" ; shift # rotates the first argument to the back
-  # continue to rotate until the steam wrapper is again the first argument
-  while [ "$1" != "$steam_first_wrapper" ]
-  do
-    if [ "$1" != "${1%"/${executable_name}"}" ] # insert this script to before BattleTech gets rotated
-    then
-      set -- "$@" "${BASEDIR}/run.sh"
+echo "cmdline:"
+echo "$@"
+
+if (( $# > 0 ))
+then # only do this complicated logic if there are arguments
+  argc=$#
+  i=0
+  inserted_run=0
+
+  while (( i < argc ))
+  do  # continue rotating until all args processed
+    arg="$1"
+    shift
+    (( ++i ))
+
+    if [[ -x "$arg" && "$(basename "$arg")" == "$executable_name" ]]
+    then  # found executable
+      if (( i == 1 ))
+      then # just set executable_path and break, no need to delay startup
+        executable_path="$arg"
+        break
+      else # need to delay startup, so insert run.sh before executable
+        inserted_run=1
+        set -- "$@" "${BASEDIR}/run.sh" "$arg"
+      fi
+    else
+      # rotate arguments
+      set -- "$@" "$arg"
     fi
-    set -- "$@" "$1" ; shift # rotates the first argument to the back
   done
-  exec "$@"
+
+  if (( inserted_run ))
+  then
+    echo "new cmdline:"
+    echo "$@"
+    exec "$@"
+  fi
 fi
+
+# Setup logging to file and terminal
+#logfile="${BASEDIR}/run.log"
+#exec > >(tee -a "$logfile") 2>&1
 
 # get doorstop settings for linux/mac from the ini (why does doorstop not do this for us?)
 doorstop_config() { grep "^${1}=" "${BASEDIR}/doorstop_config.ini" | cut -d= -f2- ; }
@@ -41,13 +63,6 @@ export DOORSTOP_ENABLED="$(doorstop_config enabled | doorstop_convert_bool)"
 export DOORSTOP_TARGET_ASSEMBLY="$(doorstop_config target_assembly | doorstop_convert_path)"
 export DOORSTOP_MONO_DLL_SEARCH_PATH_OVERRIDE="$(doorstop_config dll_search_path_override | doorstop_convert_path)"
 
-# check if the first parameter is the executable, e.g. as forwarded through Steam
-if [ -n "${1:-}" ] && [ -x "$1" ]
-then
-  executable_path="$1"
-  shift
-fi
-
 os_type="$(uname -s)"
 case ${os_type} in
   Linux*)
@@ -59,15 +74,12 @@ case ${os_type} in
     fi
     set -- "$executable_path" "$@"
 
-    # TERM=xterm
     #Fix for Mono error On Ubuntu 22.04 LTS and probably others 'System.ConsoleDriver' threw an exception. ---> System.Exception: Magic number is wrong: 542
     #Fix discussion at https://stackoverflow.com/questions/49242075/mono-bug-magic-number-is-wrong-542
+    export TERM="xterm"
 
-    set -- env \
-      TERM="xterm" \
-      LD_LIBRARY_PATH="${BASEDIR}" \
-      LD_PRELOAD="libdoorstop.so${LD_PRELOAD:+:${LD_PRELOAD}}" \
-      "$@"
+    export LD_LIBRARY_PATH="${BASEDIR}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+    export LD_PRELOAD="libdoorstop.so${LD_PRELOAD:+:${LD_PRELOAD}}"
 
     # disable ASLR which causes issues with runtime patching on some systems
     if [ "${MODTEK_DISABLE_ASLR:-}" = "true" ]
@@ -93,18 +105,15 @@ case ${os_type} in
     fi
     set -- "$executable_path" "$@"
 
-    doorstop_library_path="${BASEDIR}${DYLD_LIBRARY_PATH:+:${DYLD_LIBRARY_PATH}}"
-    doorstop_insert_libraries="libdoorstop.dylib${DYLD_INSERT_LIBRARIES:+:${DYLD_INSERT_LIBRARIES}}"
+    doorstop_insert_libraries="${BASEDIR}/libdoorstop.dylib${DYLD_INSERT_LIBRARIES:+:${DYLD_INSERT_LIBRARIES}}"
     if [ "$(uname -m)" = "x86_64" ]
     then
       set -- env \
-        DYLD_LIBRARY_PATH="${doorstop_library_path}" \
         DYLD_INSERT_LIBRARIES="${doorstop_insert_libraries}" \
         "$@"
     else
       # on Apple Silicon, run the game in x86_64 mode and pass in all required env variables
       set -- arch -x86_64 \
-        -e DYLD_LIBRARY_PATH="${doorstop_library_path}" \
         -e DYLD_INSERT_LIBRARIES="${doorstop_insert_libraries}" \
         "$@"
     fi
@@ -123,5 +132,4 @@ case ${os_type} in
   ;;
 esac
 
-echo "args3:" "$@"
 exec "$@"
